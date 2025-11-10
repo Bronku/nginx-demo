75367b4c445e41825c24660d552c1e347167639c
bronku <bronk.jakub@proton.me>
2025-11-10 16:08:39 +0100

    updated script

--- install-docker.sh
+++ install-docker.sh
@@ -1,15 +1,25 @@
 # Add Docker's official GPG key:
+echo ==> apt-get update
 sudo apt-get update
+echo ==> apt-get install ca-certificates curl
 sudo apt-get install ca-certificates curl
+echo ==> install -m 0755 -d /etc/apt/keyrings
 sudo install -m 0755 -d /etc/apt/keyrings
+echo ==> curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+echo ==> chmod a+r /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
+echo ==> add the repo to apt sources
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo ==> apt-get update
 sudo apt-get update
+echo ==> apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo ==> systemctl start docker
 sudo systemctl start docker
